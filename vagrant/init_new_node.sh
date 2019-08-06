03e3ef6e2d334378b333d55db5f2c9e63a6f5e16
Neraud <neraud.login@gmail.com>
2019-08-06 20:16:23 +0200

    Bump Vagrant box to debian/contrib-buster64 v10.0.0

--- vagrant/init_new_node.sh
+++ vagrant/init_new_node.sh
@@ -14,10 +14,7 @@ echo "=================================================="
 export DEBIAN_FRONTEND=noninteractive
 
 echo " - update"
-#apt-get -y update
-# Buster is now stable, but the base vagrant box hasn't been rebuilt. To avoid apt-get errors, we allow a releaseinfo change
-# TODO : to remove once the vagrant box is rebuilt
-apt-get -y --allow-releaseinfo-change update
+apt-get -y update
 
 echo " - upgrade"
 apt-get -q -y upgrade
