628aa10cfdaf5349756f48f3930e61f5bd7afa92
Neraud <neraud.login@gmail.com>
2019-08-13 22:05:39 +0200

    Improve generate-mixin helper to "convert" the files to how we store them in the repo

--- helpers/mixin-generator/generate-mixin.sh
+++ helpers/mixin-generator/generate-mixin.sh
@@ -2,3 +2,7 @@
 
 docker build --tag mixin-builder .
 docker run --rm -it -v $(pwd)/out:/out mixin-builder
+
+# Now that mixins are generated, we "convert" them to the way we store them in our repo.
+# The output has the same folder structure as the target to allow for easy diffs.
+./convert.sh
