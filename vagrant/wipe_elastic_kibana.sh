e7329fcb218cc7ad329a193eb636fe6072e8bd1e
Neraud <neraud.login@gmail.com>
2019-11-10 13:45:59 +0100

    Clean labels for logging-elasticsearch

--- vagrant/wipe_elastic_kibana.sh
+++ vagrant/wipe_elastic_kibana.sh
@@ -7,12 +7,12 @@ su user -c "kubectl delete namespace logging-kibana"
 
 echo ""
 echo "Release the PVs"
-su user -c "kubectl patch pv elasticsearch-data -p '{\"spec\":{\"claimRef\": null}}'"
+su user -c "kubectl patch pv elasticsearch-k8s-data -p '{\"spec\":{\"claimRef\": null}}'"
 
 echo ""
 echo "Mount Elasticsearch volume"
-mkdir -p /data/volumes/elasticsearch-data
-mount -t glusterfs master-1:/elasticsearch-data /data/volumes/elasticsearch-data
+mkdir -p /data/volumes/elasticsearch-k8s-data
+mount -t glusterfs master-1:/elasticsearch-k8s-data /data/volumes/elasticsearch-k8s-data
 
 echo ""
 echo "Wait to be sure Elasticsearch is stopped"
@@ -20,7 +20,7 @@ sleep 5
 
 echo ""
 echo "Wipe Elasticsearch volumes"
-rm -Rf /data/volumes/elasticsearch-data/*
+rm -Rf /data/volumes/elasticsearch-k8s-data/*
 
 echo ""
 echo "Provision ElasticSearch & Kibana"
