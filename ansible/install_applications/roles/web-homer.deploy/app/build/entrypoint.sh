654c49155b2495739b7e18e30f59818f5a9fc547
Neraud <neraud.login@gmail.com>
2020-02-08 15:56:57 +0100

    Add a way to pre-download logos for Homer

--- ansible/install_applications/roles/web-homer.deploy/app/build/entrypoint.sh
+++ ansible/install_applications/roles/web-homer.deploy/app/build/entrypoint.sh
@@ -15,5 +15,17 @@ else
     rm -f /tmp/nginx/conf/20-homer-context.conf
 fi
 
+mkdir -p /tmp/homer/downloaded_assets
+if [ -f /opt/images.csv ]; then
+    echo "Downloading images"
+    while IFS= read -r line; do
+        fileName=$(echo $line | cut -d";" -f1)
+        filePath=/opt/homer/assets/download/$fileName
+        url=$(echo $line | cut -d";" -f2-)
+        echo " - $fileName from $url"
+        wget -q -O $filePath $url
+    done </opt/images.csv
+fi
+
 echo "Stating nginx"
 nginx -g "daemon off;"
