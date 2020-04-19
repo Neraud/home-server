5c2c55a4c1266f6d8918adc943430f56795fc5b7
Neraud <neraud.login@gmail.com>
2020-04-19 14:05:06 +0200

    Bump logging-kibana to 1.6.0

--- vagrant/wipe_elastic_kibana.sh
+++ vagrant/wipe_elastic_kibana.sh
@@ -6,9 +6,18 @@ su user -c "kubectl delete namespace logging-elasticsearch"
 su user -c "kubectl delete namespace logging-kibana"
 
 echo ""
+echo "Release the PVs"
+su user -c "kubectl patch pv elasticsearch-k8s-data -p '{\"spec\":{\"claimRef\": null}}'"
+su user -c "kubectl patch pv kibana-data -p '{\"spec\":{\"claimRef\": null}}'"
+
+echo ""
 echo "Mount Elasticsearch volume"
 ssh 192.168.100.12 "mkdir -p /data/volumes/elasticsearch-k8s-data"
 ssh 192.168.100.12 "mount /dev/data_vg/local_elasticsearch-k8s-data /data/volumes/elasticsearch-k8s-data"
+
+echo "Mount Kibana volume"
+mkdir -p /data/volumes/kibana-data
+mount -t glusterfs master-1:/kibana-data /data/volumes/kibana-data
 
 echo ""
 echo "Wait to be sure Elasticsearch is stopped"
@@ -17,6 +26,8 @@ sleep 5
 echo ""
 echo "Wipe Elasticsearch volumes"
 ssh 192.168.100.12 "rm -Rf /data/volumes/elasticsearch-k8s-data/*"
+echo "Wipe Kibana volumes"
+rm -Rf /data/volumes/kibana-data/*
 
 echo ""
 echo "Provision ElasticSearch & Kibana"
