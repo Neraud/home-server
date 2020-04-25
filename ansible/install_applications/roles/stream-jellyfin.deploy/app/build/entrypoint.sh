c9df5f651ecae66772487ee7e58f36b6d4d2abac
Neraud <neraud.login@gmail.com>
2020-04-25 13:58:04 +0200

    Add plugins support for jellyfin and enable anime plugin

--- ansible/install_applications/roles/stream-jellyfin.deploy/app/build/entrypoint.sh
+++ ansible/install_applications/roles/stream-jellyfin.deploy/app/build/entrypoint.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
+echo "Deploying plugins"
+mkdir -p /config/plugins
+for plugin_dir in /opt/jellyfin/plugins/* ; do
+    plugin_name=$(basename "$plugin_dir")
+    echo " - $plugin_name"
+    if [ -d "/config/plugins/$plugin_name" ] ; then
+        echo "removing old version"
+        rm -Rf "/config/plugins/$plugin_name"
+    fi
+    cp -R "/opt/jellyfin/plugins/$plugin_name" "/config/plugins/$plugin_name"
+done
+
 echo "Starting jellyfin"
-dotnet /jellyfin/jellyfin.dll --datadir /config --cachedir /cache
+/jellyfin/jellyfin --datadir /config --cachedir /cache
