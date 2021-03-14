68581bcc235f73f8558c085840e7393e5bde6398
Neraud <neraud.login@gmail.com>
2021-03-14 23:40:03 +0100

    Fix new ansible version failing to build

--- install_controller_requirements.sh
+++ install_controller_requirements.sh
@@ -6,6 +6,10 @@ PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 echo "Install pip3 and virtualenv"
 apt-get -q -y install python3-pip python3-venv
 
+echo "Install ansible requirements"
+# rustc for python cryptography module
+apt-get -q -y install rustc libssl-dev
+
 echo "Create and activate ansible virtual env"
 python3 -m venv $ANSIBLE_VENV_PATH
 source $ANSIBLE_VENV_PATH/bin/activate
