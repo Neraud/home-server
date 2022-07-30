1ca9f1e2fd0654955081b3834c8fef88b1ad72b3
Neraud <neraud.login@gmail.com>
2022-07-30 13:54:50 +0200

    Use NFS for nextcloud data

--- vagrant/wipe_nextcloud.sh
+++ vagrant/wipe_nextcloud.sh
@@ -7,16 +7,14 @@ su user -c "kubectl delete namespace tool-nextcloud"
 echo ""
 echo "Release the PVs"
 su user -c "kubectl patch pv nextcloud-app -p '{\"spec\":{\"claimRef\": null}}'"
-su user -c "kubectl patch pv nextcloud-data -p '{\"spec\":{\"claimRef\": null}}'"
+su user -c "kubectl patch pv tool-nextcloud.nfs-nextcloud -p '{\"spec\":{\"claimRef\": null}}'"
 su user -c "kubectl patch pv nextcloud-mysql -p '{\"spec\":{\"claimRef\": null}}'"
 
 echo ""
 echo "Mount Nextcloud volumes"
 mkdir -p /data/volumes/nextcloud-app
-mkdir -p /data/volumes/nextcloud-data
 mkdir -p /data/volumes/nextcloud-mysql
 mount -t glusterfs master-test-1:/nextcloud-app /data/volumes/nextcloud-app
-mount -t glusterfs master-test-1:/nextcloud-data /data/volumes/nextcloud-data
 mount -t glusterfs master-test-1:/nextcloud-mysql /data/volumes/nextcloud-mysql
 
 echo ""
@@ -26,8 +24,8 @@ sleep 5
 echo ""
 echo "Wipe Nextcloud volumes"
 rm -Rf /data/volumes/nextcloud-app/*
-rm -Rf /data/volumes/nextcloud-data/*
 rm -Rf /data/volumes/nextcloud-mysql/*
+ssh 192.168.100.11 "rm -Rf /opt/mock_nas/Nextcloud/data/* ; rm -Rf /opt/mock_nas/Nextcloud/data/.*"
 
 echo ""
 echo "Provision Nextcloud"
