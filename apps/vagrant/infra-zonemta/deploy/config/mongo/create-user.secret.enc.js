241783897e0e1b50bbc46636cebf1832a9e6293e
Neraud <neraud.login@gmail.com>
2023-07-02 15:07:43 +0200

    Migrate infra-zonemta to kustomize

---
+++ apps/vagrant/infra-zonemta/deploy/config/mongo/create-user.secret.enc.js
@@ -0,0 +1,20 @@
+{
+	"data": "ENC[AES256_GCM,data:WR6wW7PKYCqmcHxoiNvaM+ZGHW8COlcskjO5DUb6NseDMpeGqUwlLIgldXVVipvVrWeh8sJhYvEA/YjXVWxZ6hiXj18+fJPL4VLZtw1x0Namz6IgQbyIeXsm2mzUSccKUYDbkNVwtb38HuVq3CgWXK86iQHhF7I=,iv:4doIfAwJYCAHYJCSFUMYzWWDCVDy5WMIF8Ney6YyUWg=,tag:mcgAPDyR7xImnufqEoImeg==,type:str]",
+	"sops": {
+		"kms": null,
+		"gcp_kms": null,
+		"azure_kv": null,
+		"hc_vault": null,
+		"age": [
+			{
+				"recipient": "age1ykc9cp7e7sylr09ldufwd337h6kvghn8ga250unudvxxlaczpuzsv04rqn",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSB5OTFIM2FNMzN3ZUFkZ29H\nNVNSOVVoYitHaHd2ZThDbWFxaXhlZFZGcDB3CkFHeUVnR3VMUmtRZnQyZ0lOcmFt\nVDlmNVJ0Y1FMTmhUSzFVTDJxMEl0eGMKLS0tIFFRSm5qeGN6MXpVbDVzU0JsSU1F\nM2xpaUI2R3lYMDhKTGNYc2NLT3AxaTAKsNhplGx5wXarGmFr55/5HHgPAr2izTgY\nlk0pakUT86+lMWrBfyNIQH/g5ryWFNKs2MXXcQJsanv7Q24hob3PTw==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2023-07-02T12:02:42Z",
+		"mac": "ENC[AES256_GCM,data:JvHKRCPKJKMdiawdgkhJyroeqFjDiKxpGq7DZMhG6jDHLZaZ5x/z6eHvpEntcQHg9aOl2QKCZWqfIsEsRs5bKHm3AVMucJDcoedxpPmTNstYGZcGCP226eFD+XINyRn91uzhHip3zo+kpsYdhYPAyPzt2YvFGunvK1BfbETgbqk=,iv:pcn53gn/yp4cEZlmxBjIF5ex0c95zF9FF8A2It02Dmc=,tag:Xc8P5cNoBUdbfvAKoaCkUQ==,type:str]",
+		"pgp": null,
+		"unencrypted_regex": "^(apiVersion|metadata|kind|type)$",
+		"version": "3.7.3"
+	}
+}
