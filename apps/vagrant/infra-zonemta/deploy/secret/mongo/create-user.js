369853fee41796ff31a135600962b5996f196bb4
Neraud <neraud.login@gmail.com>
2023-08-05 23:25:44 +0200

    Add infra-zonemta default zone configuration in overlay

--- apps/vagrant/infra-zonemta/deploy/secret/mongo/create-user.js
+++ apps/vagrant/infra-zonemta/deploy/secret/mongo/create-user.js
@@ -1,5 +1,5 @@
 {
-	"data": "ENC[AES256_GCM,data:WR6wW7PKYCqmcHxoiNvaM+ZGHW8COlcskjO5DUb6NseDMpeGqUwlLIgldXVVipvVrWeh8sJhYvEA/YjXVWxZ6hiXj18+fJPL4VLZtw1x0Namz6IgQbyIeXsm2mzUSccKUYDbkNVwtb38HuVq3CgWXK86iQHhF7I=,iv:4doIfAwJYCAHYJCSFUMYzWWDCVDy5WMIF8Ney6YyUWg=,tag:mcgAPDyR7xImnufqEoImeg==,type:str]",
+	"data": "ENC[AES256_GCM,data:FVGUEzRZwOK05kYdhrOWifLsAzCSBw5bOuX93ViqyNygFVnVeW+vl6xvndenBxBH1xA1nugDExe4JY3aducWKRkhOdDTrsUpA7cMiVsZg7m9floz12cWwvZ/ATV4x/goHzvB2SxEfhxFNAglISGPoTJiywm2PI4=,iv:dqYxXNsrVzgtAJJRpb8LaXO2n62WD3kT0IRyygbmm44=,tag:qx/KnC5vBtChbaM+AfEY6g==,type:str]",
 	"sops": {
 		"kms": null,
 		"gcp_kms": null,
@@ -8,11 +8,11 @@
 		"age": [
 			{
 				"recipient": "age1ykc9cp7e7sylr09ldufwd337h6kvghn8ga250unudvxxlaczpuzsv04rqn",
-				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSB5OTFIM2FNMzN3ZUFkZ29H\nNVNSOVVoYitHaHd2ZThDbWFxaXhlZFZGcDB3CkFHeUVnR3VMUmtRZnQyZ0lOcmFt\nVDlmNVJ0Y1FMTmhUSzFVTDJxMEl0eGMKLS0tIFFRSm5qeGN6MXpVbDVzU0JsSU1F\nM2xpaUI2R3lYMDhKTGNYc2NLT3AxaTAKsNhplGx5wXarGmFr55/5HHgPAr2izTgY\nlk0pakUT86+lMWrBfyNIQH/g5ryWFNKs2MXXcQJsanv7Q24hob3PTw==\n-----END AGE ENCRYPTED FILE-----\n"
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBNVkpSV3dud0lqa0c1eHY5\neGVZMjBzWjd2NitlUEFFbWxnWU9WTFJib0VFCkFsSkVoeGJXbGt0Wm9OSUhCV3ow\nMDdFVkxlY0dpc3dPRTVnMWtjeEh2TUEKLS0tIEVEeURtSUtXeEpiTXU1MHFqekE3\ncmNDdzg5QkI3c2VmUlo3UnFzaWREcWsKZN87L5GQ0LZ8mYvkuY3bpMbBWFQAj7PX\nCFZ2WFB0WqlI3l0dBkKonMnwC3/C8jefIaVZqOTPmly49Yr8Rc8uVg==\n-----END AGE ENCRYPTED FILE-----\n"
 			}
 		],
-		"lastmodified": "2023-07-02T12:02:42Z",
-		"mac": "ENC[AES256_GCM,data:JvHKRCPKJKMdiawdgkhJyroeqFjDiKxpGq7DZMhG6jDHLZaZ5x/z6eHvpEntcQHg9aOl2QKCZWqfIsEsRs5bKHm3AVMucJDcoedxpPmTNstYGZcGCP226eFD+XINyRn91uzhHip3zo+kpsYdhYPAyPzt2YvFGunvK1BfbETgbqk=,iv:pcn53gn/yp4cEZlmxBjIF5ex0c95zF9FF8A2It02Dmc=,tag:Xc8P5cNoBUdbfvAKoaCkUQ==,type:str]",
+		"lastmodified": "2023-08-05T21:22:03Z",
+		"mac": "ENC[AES256_GCM,data:iF94XD3myIPsfqJEr4cXubXG9wRGKUq7itmZEurDRyGCP0FK/b7xKeMsWJ/uNrt8iLQRUOMOdgOgv3bW/K3Tn69D93V2QYo/5nBa9nOvgHOBOVrT5oru8tL1a/EkFs0o3wuVPY4HveFGlzG/pWGig+TdXPOcQns9d/m1l1yYE94=,iv:iVY1ha2Bb9Mg1vpBkMTK0HupkY6nVTUaOmUqfNRyJoM=,tag:1DRpem1qPFxPffo22ErTsw==,type:str]",
 		"pgp": null,
 		"unencrypted_regex": "^(apiVersion|metadata|kind|type)$",
 		"version": "3.7.3"
