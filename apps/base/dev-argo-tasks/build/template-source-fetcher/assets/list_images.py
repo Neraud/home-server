1adb1b23e1623de884095258da176afcc2c8bcf1
Neraud <neraud.login@gmail.com>
2023-08-09 22:59:33 +0200

    Fix workflow fails when build folder doesn't exist

--- apps/base/dev-argo-tasks/build/template-source-fetcher/assets/list_images.py
+++ apps/base/dev-argo-tasks/build/template-source-fetcher/assets/list_images.py
@@ -13,24 +13,29 @@ parser.add_argument('--out', '-o', nargs='?', dest='out_file', help='Target JSON
 args = parser.parse_args()
 images_to_build = []
 
-print(f'Looking for images to build under {args.in_folder}')
 
-for f in [ f.name for f in os.scandir(args.in_folder) if f.is_dir() ]:
-    print(f' - {f}')
-    vars_file_path = os.path.join(args.in_folder, f, 'vars.yaml')
-    if os.path.exists(vars_file_path):
-        with open(vars_file_path) as yf:
-            data = yaml.load(yf, Loader=SafeLoader)
-            print(data)
-            image_to_build = {
-                "folder": f,
-                "image_name": data["image_name"],
-                "image_tag": data["image_tag"],
-            }
-            images_to_build.append(image_to_build)
-    else:
-        print('Missing {vars_file_path} !')
-        sys.exit(1)
+if os.path.exists(args.in_folder):
+    print(f'Looking for images to build under {args.in_folder}')
+
+    for f in [ f.name for f in os.scandir(args.in_folder) if f.is_dir() ]:
+        print(f' - {f}')
+        vars_file_path = os.path.join(args.in_folder, f, 'vars.yaml')
+        if os.path.exists(vars_file_path):
+            with open(vars_file_path) as yf:
+                data = yaml.load(yf, Loader=SafeLoader)
+                print(data)
+                image_to_build = {
+                    "folder": f,
+                    "image_name": data["image_name"],
+                    "image_tag": data["image_tag"],
+                }
+                images_to_build.append(image_to_build)
+        else:
+            print('Missing {vars_file_path} !')
+            sys.exit(1)
+else:
+    print(f'In folder {args.in_folder} doesn\'t exist, nothing to build')
+
 
 print('')
 
